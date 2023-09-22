b9a82fa63cedb063069de8abf099b96235ec3556
MaximovInk <maximovink.ru@gmail.com>
2023-09-23 02:54:53 +0300

    added GroundManager
    - added groundManager for swap ground materials

--- Assets/Demolish/Scripts/Managers/GroundManager.cs
+++ Assets/Demolish/Scripts/Managers/GroundManager.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 
-namespace Assets.Demolish.Scripts.Managers
+namespace MaximovInk
 {
-    internal class GroundManager
+    public class GroundManager : MonoBehaviourSingleton<GroundManager>
     {
+        [SerializeField] private Material[] _materials;
+
+        [SerializeField] private int _currentMaterialIndex = 0;
+
+        [SerializeField] private GameObject _ground;
+
+        private MeshRenderer _groundRenderer;
+
+        private void Awake()
+        {
+            _groundRenderer = _ground.GetComponent<MeshRenderer>();
+
+            ApplyCurrentMaterial();
+        }
+
+        private void ApplyCurrentMaterial()
+        {
+            if (_materials.Length == 0) return;
+
+            _currentMaterialIndex = Mathf.Clamp(_currentMaterialIndex, 0, _materials.Length - 1);
+
+            _groundRenderer.sharedMaterial = _materials[_currentMaterialIndex];
+        }
     }
 }
