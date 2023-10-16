669a5f1d93b8a791184592f6c683f2bed85afa1e
MaximovInk <maximovink.ru@gmail.com>
2023-10-17 00:35:37 +0300

    - orientation ui elements (building state)
    - two canvas objects for portrait/lanscape orientations

---
+++ Assets/Demolish/Scripts/UI/OrientationToggle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace MaximovInk
+{
+    public class OrientationToggle : MonoBehaviour
+    {
+        [SerializeField] private GameObject _landscape;
+        [SerializeField] private GameObject _portrait;
+
+        private void Awake()
+        {
+            var isPortrait = Screen.width < Screen.height;
+
+            _landscape.SetActive(!isPortrait);
+            _portrait.SetActive(isPortrait);
+        }
+    }
+}
