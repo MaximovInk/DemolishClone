07273198e6cdb04fbb143c66497f4088a58893f4
MaximovInk <maximovink.ru@gmail.com>
2023-10-17 00:43:40 +0300

    - gamePush pause/gameplay events

---
+++ Assets/Demolish/Scripts/Managers/GlobalAPIManager.cs
@@ -0,0 +1,25 @@
+﻿using GamePush;
+
+namespace MaximovInk
+{
+    internal class GlobalAPIManager : MonoBehaviourSingleton<GlobalAPIManager>
+    {
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            DontDestroyOnLoad(this);
+            GP_Game.GameplayStart();
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance != null && Instance == this)
+                GP_Game.GameplayStop();
+        }
+    }
+}
---
+++ Assets/Demolish/Scripts/UI/PausePanel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MaximovInk
+{
+    public class PausePanel : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            GamePush.GP_Game.Pause();
+        }
+
+        private void OnDisable()
+        {
+            GamePush.GP_Game.Resume();
+        }
+    }
+}
